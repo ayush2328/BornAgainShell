728df357b6743ba35685a61aba5862be52db7768
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-12-17 19:06:54 +0530

    Constant Varables

--- Variables/UserDefined.sh
+++ Variables/UserDefined.sh
@@ -1,6 +1,5 @@
 # /bin/bash
 # <-----User Defined variables------>
-today=$(date)
-echo "Today date is : $today"
-calendra=$(cal 2025)
-echo "This year calendra is: $calendra"
+# Constant Variables
+readonly college=SRM
+echo "College Name: $college"
