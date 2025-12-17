86c19494d16b71fb8054811294d1c4acaf0409eb
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-12-17 23:36:17 +0530

    Predfined Variables

--- Variables/SystemDefined.sh
+++ Variables/SystemDefined.sh
@@ -1,2 +1,13 @@
 # !/bin/bash
 env
+# Kali Linex Predfined Variables
+# | Variable | Meaning                           |
+# | -------- | --------------------------------- |
+# | `$0`     | Script name                       |
+# | `$1`     | First argument                    |
+# | `$2`     | Second argument                   |
+# | `$#`     | Total arguments                   |
+# | `$@`     | All arguments                     |
+# | `$$`     | Process ID                        |
+# | `$?`     | Last command status (0 = success) |
+echo $$ $0 $#
